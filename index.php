4856299bc53baf4e686fa4966642772b3aadcf29
Guy Routledge <guy@bigfish.co.uk>
2013-01-09 21:48:08 +0000

    add boilerplate code to index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Which Platform? at Clapham Junction</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+
+        <link rel="stylesheet" href="css/screen.css">
+    </head>
+    <body>
+        
+        <?php // Scripts at the bottom for performance ?>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"></script>
+        <script src="js/app.js"></script>
+
+        <?php // Google Analytics: change UA-XXXXX-X to be your site's ID. ?>
+        <script>
+            var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+            g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+            s.parentNode.insertBefore(g,s)}(document,'script'));
+        </script>
+    </body>
+</html>
