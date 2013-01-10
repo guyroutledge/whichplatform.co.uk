748d255d4395589f9769de75c167d23d197a7bf7
Guy Routledge <guy@bigfish.co.uk>
2013-01-10 13:53:02 +0000

    create includes for header and footer

---
+++ inc/footer.php
@@ -0,0 +1,18 @@
+		<footer class="app-footer">
+			<?php // copyright, sharing links, attribution, licensing ?>
+		</footer>
+
+        
+		<?php // Scripts at the bottom for performance ?>
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"></script>
+		<script src="js/app.js"></script>
+
+		<?php // Google Analytics: change UA-XXXXX-X to be your site's ID. ?>
+		<script>
+			var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+			(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+			g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+			s.parentNode.insertBefore(g,s)}(document,'script'));
+		</script>
+	</body>
+</html>
---
+++ inc/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Which Platform? at Clapham Junction</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+
+        <link rel="stylesheet" href="css/screen.css">
+    </head>
+    <body>
+
+    	<header class="app-header">
+	        
+	        <nav class="nav-menu">
+	            <a href="#"><i class="icon-nav"></i> menu</a>
+			</nav>
+
+			<hgroup class="title">
+				<h1>Which Platform<span>?</span></h1>
+				<h2>at Clapham Junction</h2>
+			</hgroup>
+
+		</header>
