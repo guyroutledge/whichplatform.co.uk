3e7629580dae14939018b6b0f0ae17be852b838c
Guy Routledge <guy@bigfish.co.uk>
2013-02-10 07:34:35 +0000

    Create app object to wrap all JS functionality
    Add placeholder object for caching data
    Get the data file via AJAX
    Init getting data on DOM ready

--- js/app.js
+++ js/app.js
@@ -1,13 +1,27 @@
 jQuery(function($){
-	
-	$('#search-field').autocomplete({
-        "data" : [
-            ['apple', 1],
-            ['apricot', 2],
-            ['pear', 3],
-            ['prume', 4],
-            ['Doyenné du Comice', 5]
-        ]
-    });
+
+	var app = {
+		data: {},
+		getData: function(){
+			$.ajax({
+				url: 'js/data.json',
+				data: null,
+				success: function(data, textStatus, jqXHR){
+					app.data = data;
+				},
+				dataType: 'json'
+			});
+		},
+		autoComplete: function(data){
+			$('#search-field').autocomplete({
+				"data" : data
+			});
+		},
+		init: function(){
+			app.getData();
+		}
+	};
+	// Kick things off
+	app.init();
 
 });
