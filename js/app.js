f7fd630951c9b728f8644c80250ef42e0d180868
Guy Routledge <guy@bigfish.co.uk>
2013-02-10 08:57:53 +0000

    Build the destinations list for autocomplete
    Loop through all the destinations in data.json and create a de-duped
    list of destinations to pass to autocomplete

--- js/app.js
+++ js/app.js
@@ -8,9 +8,46 @@ jQuery(function($){
 				data: null,
 				success: function(data, textStatus, jqXHR){
 					app.data = data;
+					app.destinations.buildList();
 				},
 				dataType: 'json'
 			});
+		},
+		destinations: {
+			list: {},
+			buildList: function(){
+				var data = app.data; // cache shorter variable name
+				var platforms = data.platforms;
+				var destinationsList = []; // an array that will eventually hold a de-duped list of all destinations
+				var tmpArr = []; // a temp array to hold all the destinations
+				var tmpObj = {}; // a temp object to produce a de-duped list of destinations
+				var destination; // placeholder for building destinationsList
+				var i, j, k; // counters
+
+				// Create an array of all the destinations, including
+				// duplicates by looping through each platform
+				for (i = 0; i < platforms.length; i++) {
+					for (j = 0; j < platforms[i].destinations.length; j++) {
+						tmpArr.push(platforms[i].destinations[j]);
+					}
+				}
+				// Add each destination from tmpArr as a key to tmpObj
+				// as you cannot have more than one property with the
+				// same key, the destinations are deduped.
+				for (k = 0; k < tmpArr.length; k++) {
+					tmpObj[tmpArr[k]] = true;
+				}
+				// Turn tmpObj back into an array of destinations to
+				// pass as local data to jquery.autocomplete
+				for ( destination in tmpObj ) {
+					destinationsList.push(destination);
+				}
+				app.destinations.list = destinationsList;
+				app.autoComplete(destinationsList);
+			},
+			displayDestinations: {
+
+			}
 		},
 		autoComplete: function(data){
 			$('#search-field').autocomplete({
